c93b17e049ebd5595168ab7e81df2a8376efc162
Karthick Kumaran <asvkarthick@gmail.com>
2024-04-07 22:56:20 -0700

    Simple pytorch program to demonstrate empty tensors with different dimensions

---
+++ 01-basics/002-tutorial.py
@@ -0,0 +1,10 @@
+import torch
+
+x_1d = torch.empty(3)
+print("x_1d : {}".format(x_1d))
+
+x_2d = torch.empty(3, 3)
+print("x_2d : {}".format(x_2d))
+
+x_3d = torch.empty(3, 3, 3)
+print("x_3d : {}".format(x_3d))
