52e580dc96715941a1ab2ab4de8f6cf0c03ff1a4
Karthick Kumaran <asvkarthick@gmail.com>
2024-04-07 22:58:03 -0700

    Program to do arithmetic operations

---
+++ 01-basics/004-tutorial.py
@@ -0,0 +1,9 @@
+import torch
+
+x = torch.rand(3, 3)
+y = torch.rand(3, 3)
+print("X : {}".format(x))
+print("Y : {}".format(y))
+print("X + Y : {}".format(x + y))
+print("X - Y : {}".format(x - y))
+print("X * Y : {}".format(x * y))
